362b968e9888ecab9838ec7f95586881cbf3c911
IncogOwl <97695545+IncogOwl@users.noreply.github.com>
2022-01-20 22:59:53 +0400

    Finding a breaking test

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,12 +11,14 @@ public class MarkdownParse {
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            System.out.println("first "+ currentIndex);
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            System.out.println("second "+ currentIndex);
         }
         return toReturn;
     }
