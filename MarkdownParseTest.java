d2137e3903d842a6acc4986545903c875f5f5aa5
IncogOwl <97695545+IncogOwl@users.noreply.github.com>
2022-01-27 11:31:53 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,10 +12,31 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void testGetLinks() throws IOException{
+    public void testGetLinksTestFile() throws IOException{
         Path fileName= Path.of("test-file.md");
         String contents=Files.readString(fileName);
         assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
+
+    }
+    @Test
+    public void testGetLinksNewFile() throws IOException{
+        Path fileName= Path.of("new-file.md");
+        String contents=Files.readString(fileName);
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
+
+    }
+    @Test
+    public void testGetLinksEmptyFile() throws IOException{
+        Path fileName= Path.of("empty-file.md");
+        String contents=Files.readString(fileName);
+        assertEquals(List.of(), MarkdownParse.getLinks(contents));
+
+    }
+    @Test
+    public void testGetLinksBracketFile() throws IOException{
+        Path fileName= Path.of("bracket-file.md");
+        String contents=Files.readString(fileName);
+        assertEquals(List.of(), MarkdownParse.getLinks(contents));
 
     }
 }
